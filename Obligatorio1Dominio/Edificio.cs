ec2d28a17e1f69950d1222c371b3a24e0ae1f855
pingold16 <p.ingold@outlook.com>
2018-10-12 15:04:22 -0300

    Intento AltaApartamento
    Lo tome en sistema y se lo mande a edificio para hacer el alta en edificio que es quien tiene la lista.

--- Obligatorio1Dominio/Edificio.cs
+++ Obligatorio1Dominio/Edificio.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Obligatorio1Dominio
 {
@@ -17,11 +18,46 @@ namespace Obligatorio1Dominio
         }
         public List<Apartamento> Apartamentos {
             get { return apartamentos; }
+            set { apartamentos = value; }
         }
         internal void ModificarDatos(string nombre, string direccion)
         {
             this.nombre = nombre;
             this.direccion = direccion;
+        }
+
+        internal string AltaApartamento(string piso, int numero, int metraje, int precioBase, string orientacion)
+        {
+            string mensaje = "";
+            if (piso != "" && numero != 0 && metraje != 0 && precioBase != 0 && orientacion != "")//Primer paso verificar que no sea vacio.
+            {
+                if (BuscarApartamento(piso, numero) == null)//Segundo paso verificar que no existe.
+                {
+                    Apartamento n = new Apartamento(); //Paso iniciar el objeto.
+                    apartamentos.Add(n);
+                    n.ModificarDatos(piso, numero, metraje, precioBase, orientacion);
+                }
+                else { mensaje = "El edificio ya existe"; }
+            }
+            else { mensaje = "Los valores son vacios."; }
+            return mensaje;
+        }
+
+        public object BuscarApartamento(string piso, int numero)
+        {
+            bool existe = false;
+            Apartamento c = null;
+            int i = 0;
+            while (i < apartamentos.Count && !existe)
+            {
+                if (apartamentos[i].Piso.ToUpper() == piso.ToUpper() && apartamentos[i].Numero == numero)
+                {
+                    existe = true;
+                    c = apartamentos[i];
+                }
+                i++;
+            }
+            return c;
         }
     }
 }
