d62764d60eca6caad50f00d02fef958cdf3d03b4
pingold16 <p.ingold@outlook.com>
2018-10-18 13:19:20 -0300

    ddl prueba en index

--- Obligatorio1/index.aspx.cs
+++ Obligatorio1/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Obligatorio1Dominio;
 
 namespace Obligatorio1
 {
@@ -11,7 +12,20 @@ namespace Obligatorio1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DropDownList1.DataSource = Sistema.Instancia.Edificios;
+            DropDownList1.DataTextField = "Nombre";
+            DropDownList1.DataBind();
+            DropDownList1.Items.Insert(0, "Seleccione edificio");
+
+            DropDownList2.DataSource = Sistema.Instancia.CargaAptos();
+            DropDownList1.DataTextField = "Numero";
+            DropDownList1.DataBind();
+            DropDownList1.Items.Insert(0, "Seleccione edificio");
         }
     }
 }
